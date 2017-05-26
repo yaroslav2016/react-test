2d04aa87bde495fea773ebbd66ebf1d61b5ec24e
yaroslav2016 <yaroslavrokhmanov@@ukr.net>
2017-05-26 12:27:51 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import React from 'react-dom';
+
+class App extends React.Component{
+    render(){
+        return{
+        <h1>App works</h1>
+        };
+    }
+}
+
+ReactDOM.render{
+    <App/>,
+        document.getElementById('app')
+};

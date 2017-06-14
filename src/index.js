7f0b5a7138cc64ca49abb7a9a112018cbb0fa91a
yaroslav2016 <yaroslavrokhmanov@@ukr.net>
2017-06-15 00:01:44 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hi');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        return(
+            <h1>App works!</h1>
+    );
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+);

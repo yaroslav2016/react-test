2707efa9e7ea058de81e5d6445989ffef543c40c
yaroslavrokhmanov <yaroslavrokhmanov@gmail.com>
2019-01-06 23:03:46 +0200

    close/open

--- app/src/components/Article.js
+++ app/src/components/Article.js
@@ -1,14 +1,31 @@
-import React from 'react'
-function  Article(props) {
-    const{article} = props
-    const body = <section>{article.text}</section>
-    return(
+import React, {Component} from 'react'
+
+class Article extends  Component{
+    state = {
+        isOpen: true
+    }
+render(){
+    const {article} = this.props
+    const body = this.state.isOpen&&<section>{article.text}</section>
+    return (
         <div>
-            <h2>{article.title}</h2>
+            <h2>
+                {article.title}
+                <button onClick={this.handleClick}>close</button>
+            </h2>
             {body}
             <h3>Creation date: {(new Date(article.date)).toDateString()}</h3>
         </div>
-)
+    )
+}
+
+ handleClick = () =>{
+    console.log('---', 'clicked')
+    this.setState({
+        isOpen: !this.state.isOpen
+    })
+
+}
 }
 
 export default Article

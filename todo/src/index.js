b10b81dcb606ffba2e4a2c4980cbbe1c8148ed21
yaroslavrokhmanov <yaroslavrokhmanov@gmail.com>
2019-05-18 23:23:55 +0300

    sructure

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const  el = (
+  <div>
+      <h1>Todo List</h1>
+      <input placeholder="search"/>
+      <ul>
+          <li>Learn React</li>
+          <li>Build App</li>
+      </ul>
+  </div>
+);
+
+ReactDOM.render(el,
+    document.getElementById('root'));

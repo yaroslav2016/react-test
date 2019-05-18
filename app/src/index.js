051f0462ec80817a12289ec843f926823a7db6a0
yaroslavrokhmanov <yaroslavrokhmanov@gmail.com>
2019-05-18 16:08:43 +0300

    start project

--- app/src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by user on 10.12.2018.
- */
-import React from 'react'
-import {render} from 'react-dom'
-import App from './components/App'
-
-render(<App/>, document.getElementById('root'))

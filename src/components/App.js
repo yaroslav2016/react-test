ef032ec5c7a70661068b6b9d5eb7aa6101c19942
yaroslavrokhmanov <yaroslavrokhmanov@gmail.com>
2020-01-12 00:20:16 +0200

    button dell function

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
 import React from "react";
 import Todo from "./Todo/Todo";
 
-const users = [
-    {id:1, number:"1", title: "Pol Redex"}, 
-    {id:2, number:"2", title: "Grgzeszyk Kulemski"}, 
-    {id:3, number:"3", title: "Mark Stavicz"}
-]
-
 class App extends React.Component {
+    state = {
+        todos: [
+            {id:1, number:"1", title: "Pol Redex"}, 
+            {id:2, number:"2", title: "Grgzeszyk Kulemski"}, 
+            {id:3, number:"3", title: "Mark Stavicz"}
+        ]
+     };
+ 
+     deleteTodo = (e, id) => {
+        this.setState({todos: this.state.todos.filter(todo => todo.id !== id)});
+     };
+
     render() {
         return (
         <div>
             <h1>Users:</h1>
         
-            {users.map((users) =>{
-                return <Todo key={users.id} title={users.title}/>
+            {this.state.todos.map(todo =>{
+                return (
+                <Todo 
+                key={todo.id} 
+                id={todo.id}
+                title={todo.title} 
+                deleteTodo={this.deleteTodo} 
+                />
+                );
             })
             }
         </div>
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -2,18 +2,15 @@ import React from "react";
 import "./Todo.css"
 
 class Todo extends React.Component {
-    delete = (e, title) => {
-        console.log("Delete", title);
-    }
     render() {
         return(
             <div className="todo">
                 <p>{this.props.title}</p>
-                <button onClick={(e) => this.delete(e, this.props.title)}>Delete</button>
+                <button onClick={e => this.props.deleteTodo(e, this.props.id)}>Delete</button>
             </div>
-        )
+        );
     }
-}
 
+}
 
 export default Todo;
